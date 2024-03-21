34af6b94b185483bd9d69888f91bdb24b7f83eb4
Nata Nael <natanael280198@gmail.com>
2024-03-21 22:34:31 +0700

    feat: get friend with all params

--- internal/repository/friend_repository.go
+++ internal/repository/friend_repository.go
@@ -2,12 +2,15 @@ package repository
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
 
+	"github.com/natanaelrusli/segokuning-be/internal/dto"
 	"github.com/natanaelrusli/segokuning-be/internal/model"
 )
 
 type FriendRepository interface {
-	GetFriendsByUserID(userId int64, limit int64, offset int64) ([]model.FriendUserData, error)
+	GetFriendsByUserID(friendQuery dto.FriendQuery) ([]model.FriendUserData, error)
 }
 
 type friendRepositoryImpl struct {
@@ -20,8 +23,9 @@ func NewFriendRepository(db *sql.DB) *friendRepositoryImpl {
 	}
 }
 
-func (fr *friendRepositoryImpl) GetFriendsByUserID(userId int64, limit int64, offset int64) ([]model.FriendUserData, error) {
+func (fr *friendRepositoryImpl) GetFriendsByUserID(friendQuery dto.FriendQuery) ([]model.FriendUserData, error) {
 	var users []model.FriendUserData
+	var args []interface{}
 
 	query := `
 		select 
@@ -36,17 +40,51 @@ func (fr *friendRepositoryImpl) GetFriendsByUserID(userId int64, limit int64, of
 		inner join images i on users.images_id = i.id
 		where
 			users.id is not null
+		`
+
+	if friendQuery.OnlyFriend {
+		args = append(args, int64(friendQuery.UserId))
+		query += fmt.Sprintf(" AND (f.uid1 = $%d OR f.uid2 = $%d)", len(args), len(args))
+	}
+
+	if friendQuery.Search != "" {
+		args = append(args, friendQuery.Search)
+		query += fmt.Sprintf(" AND users.name ILIKE '%%' || $%d || '%%'", len(args))
+	}
+
+	query += `
 		group by 
 			users.id,
 			users.name,
 			i.url,
 			users.images_id,
 			users.created_at
-		LIMIT $1
-		OFFSET $2;
-		`
+	`
 
-	rows, err := fr.db.Query(query, limit, offset)
+	if friendQuery.SortBy == "friendCount" {
+		query += " ORDER BY friend_count"
+	} else if friendQuery.SortBy == "createdAt" {
+		query += " ORDER BY users.created_at"
+	} else {
+		query += " ORDER BY users.created_at" // Default sorting by createdAt
+	}
+
+	if friendQuery.OrderBy == "asc" {
+		query += " ASC"
+	} else {
+		query += " DESC" // Default ordering is DESC
+	}
+
+	args = append(args, int64(friendQuery.Limit))
+	query += fmt.Sprintf(" LIMIT $%d", len(args))
+
+	args = append(args, int64(friendQuery.Offset))
+	query += fmt.Sprintf(" OFFSET $%d", len(args))
+
+	log.Println(query)
+	log.Println(args...)
+
+	rows, err := fr.db.Query(query, args...)
 	if err != nil {
 		return nil, err
 	}
